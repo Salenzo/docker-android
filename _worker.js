256381b9d047b9c9234f5160d277f8dd331f97f2
Frog Chen <satgo1546@yeah.net>
2025-04-11 20:38:43 +0800

    complete forward

--- _worker.js
+++ _worker.js
@@ -1,13 +1,7 @@
-export default {
-  async fetch(request, env) {
-    const url = new URL(request.url)
-    if (url.pathname.startsWith('/api/')) {
-      return new Response('OK')
-    }
-    if (url.pathname.startsWith('/hello/')) {
-      return env.hello.fetch(url)
-    }
-    return new Response(url)
-    // return env.ASSETS.fetch(request)
-  },
+export async function fetch(request, env) {
+  const url = new URL(request.url)
+  if (url.pathname.startsWith('/pages/')) {
+    return env.ASSETS.fetch(request)
+  }
+  return env.hello.fetch(request)
 }
