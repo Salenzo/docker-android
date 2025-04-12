bf416ce06294bf6b41521a104edc8a9a6e161712
Frog Chen <satgo1546@yeah.net>
2025-04-12 14:11:29 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,7 +1,11 @@
 export async function fetch(request, env) {
-  const url = new URL(request.url)
-  if (url.pathname.startsWith('/pages/')) {
-    return env.ASSETS.fetch(request)
+  try {
+    const url = new URL(request.url)
+    if (url.pathname.startsWith('/pages/')) {
+      return env.ASSETS.fetch(request)
+    }
+    return env.hello.fetch(request)
+  } catch (error) {
+    return new Response(String(error?.stack), { status: 500 })
   }
-  return env.hello.fetch(request)
 }
