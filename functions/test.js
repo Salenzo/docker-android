726a5323591fc65a339c9348e81d904fbeb936c2
Frog Chen <satgo1546@yeah.net>
2025-06-12 21:05:38 +0800

    import '../worker.js'

--- functions/test.js
+++ functions/test.js
@@ -1,5 +1,7 @@
+import workerJS from '../worker.js'
+
 const sha256 = async input => Array.from(new Uint8Array(await crypto.subtle.digest('SHA-256', new TextEncoder().encode(input))), b => ('0' + b.toString(16)).slice(-2)).join('')
 
 export async function onRequest(context) {
-	return new Response(await sha256(context.request.url))
+	return new Response(typeof workerJS + workerJS + await sha256(context.request.url))
 }
